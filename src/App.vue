6c3efbe34d109d908d2b749d56050fc9abdb4390
cwnnn <cangs026@gmail.com>
2025-12-21 01:52:09 +0300

    add header componenet

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,41 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { useI18n } from 'vue-i18n'
-
-const { t, locale } = useI18n()
-
-function setLang(lang: 'tr' | 'en') {
-  locale.value = lang
-}
+import AppHeader from './components/AppHeader/AppHeader.vue'
 </script>
 
 <template>
-  <header class="flex gap-4">
-    <button @click="setLang('tr')">TR</button>
-    <button @click="setLang('en')">EN</button>
-  </header>
+  <AppHeader>
+    <template #logo>
+      <img src="../public/logo.png" alt="" class="w-13" />
+      <h1 class="text-3xl font-extrabold font-accent text-(--text-header)">RY Coffe</h1></template
+    >
+    <template #nav>
+      <a
+        class="font-Header px-3 py-2 text-(--text-header) font-Header hover:bg-white/10"
+        href="#menu"
+        >Menu</a
+      >
+      <a class="font-Header px-3 py-2 text-(--text-header) hover:bg-white/10" href="#ourstory"
+        >Our Story</a
+      >
+      <a
+        class="font-Header px-3 py-2 text-(--text-header) font-Header hover:bg-white/10"
+        href="#location"
+        >Location</a
+      >
+      <a
+        class="font-Header px-3 py-2 text-(--text-header) font-Header hover:bg-white/10"
+        href="#contact"
+        >Contact</a
+      >
+    </template>
 
-  <h1 class="">{{ t('header.home') }}</h1>
-  <h2>{{ t('hero.subtitle') }}</h2>
-  <p>{{ t('hero.description') }}</p>
-
-  <button>{{ t('common.call') }}</button>
+    <template #actions>
+      <button class="rounded-lg bg-white/10 px-3 py-2 text-(--text-header) hover:bg-white/15">
+        language
+      </button>
+    </template>
+  </AppHeader>
 
   <RouterView />
 </template>
---
+++ src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="header">
+    <div class="header-wrapper container">
+      <!-- SOL -->
+      <div class="header-left">
+        <slot name="logo">
+          <!-- default logo -->
+          <div class="header-logo">⚡</div>
+          <h1 class="header-title">App Name</h1>
+        </slot>
+      </div>
+
+      <!-- SAĞ -->
+      <div class="header-right">
+        <!-- Desktop Nav -->
+        <nav class="header-nav-desktop">
+          <slot name="nav" />
+        </nav>
+
+        <!-- Actions -->
+        <slot name="actions" />
+
+        <!-- Mobile Button -->
+        <button @click="mobileOpen = !mobileOpen" class="header-mobile-btn">☰</button>
+      </div>
+    </div>
+
+    <!-- MOBILE MENU -->
+    <div v-if="mobileOpen" class="header-mobile">
+      <nav class="header-mobile-nav container">
+        <slot name="nav" />
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const mobileOpen = ref(false)
+</script>
+<style src="./AppHeader.css"></style>
