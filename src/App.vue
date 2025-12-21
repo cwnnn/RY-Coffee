47f36097cfb106db9c560c2fdf97ee257de1723b
cwnnn <cangs026@gmail.com>
2025-12-21 03:09:08 +0300

    add languagetoggle componenet and use i18n for app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,45 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import AppHeader from './components/AppHeader/AppHeader.vue'
+import LanguageToggle from './components/LanguageToggle/LanguageToggle.vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
+const { locale } = useI18n()
 </script>
 
 <template>
   <AppHeader>
     <template #logo>
-      <img src="../public/logo.png" alt="" class="w-13" />
-      <h1 class="text-3xl font-extrabold font-accent text-(--text-header)">RY Coffe</h1></template
-    >
+      <a href="#hero" class="flex items-center gap-3">
+        <img src="/logo.png" alt="RY Coffee Logo" class="w-13" />
+        <h1 class="text-3xl font-extrabold text-(--text-header)">RY Coffee</h1>
+      </a>
+    </template>
     <template #nav>
       <a
         class="font-Header px-3 py-2 text-(--text-header) font-Header hover:bg-white/10"
         href="#menu"
-        >Menu</a
+        >{{ t('header.menu') }}</a
       >
-      <a class="font-Header px-3 py-2 text-(--text-header) hover:bg-white/10" href="#ourstory"
-        >Our Story</a
-      >
+      <a class="font-Header px-3 py-2 text-(--text-header) hover:bg-white/10" href="#ourstory">{{
+        t('header.ourStory')
+      }}</a>
       <a
         class="font-Header px-3 py-2 text-(--text-header) font-Header hover:bg-white/10"
         href="#location"
-        >Location</a
+        >{{ t('header.location') }}</a
       >
       <a
         class="font-Header px-3 py-2 text-(--text-header) font-Header hover:bg-white/10"
         href="#contact"
-        >Contact</a
+        >{{ t('header.contact') }}</a
       >
     </template>
 
     <template #actions>
-      <button class="rounded-lg bg-white/10 px-3 py-2 text-(--text-header) hover:bg-white/15">
-        language
-      </button>
+      <LanguageToggle v-model="locale" option1="tr" option2="en" />
     </template>
   </AppHeader>
 
---
+++ src/components/LanguageToggle/LanguageToggle.vue
@@ -0,0 +1,38 @@
+<template>
+  <button @click="toggle" class="lang-switch">
+    <!-- Kayan knob -->
+    <span
+      class="lang-switch-knob"
+      :class="modelValue === option2 ? 'knob-right' : 'knob-left'"
+    ></span>
+
+    <!-- Option 1 -->
+    <span class="lang-switch-label left" :class="modelValue === option1 ? 'active' : 'inactive'">
+      {{ option1.toUpperCase() }}
+    </span>
+
+    <!-- Option 2 -->
+    <span class="lang-switch-label right" :class="modelValue === option2 ? 'active' : 'inactive'">
+      {{ option2.toUpperCase() }}
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: string
+  option1: string
+  option2: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+function toggle() {
+  const next = props.modelValue === props.option1 ? props.option2 : props.option1
+
+  emit('update:modelValue', next)
+}
+</script>
+<style scoped src="./LanguageToggle.css"></style>
