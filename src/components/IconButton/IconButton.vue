1652397f6e172bce93d8e7193e7ba52ac24fe6a6
cwnnn <cangs026@gmail.com>
2025-12-23 00:25:45 +0300

    add icon button component

---
+++ src/components/IconButton/IconButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <a :href="link" target="_blank" rel="noopener" class="feature-link">
+    <!-- ICON -->
+    <div class="feature-icon">
+      <slot name="icon" />
+    </div>
+
+    <h4 class="feature-title">
+      <slot name="title" />
+    </h4>
+
+    <!-- DESCRIPTION -->
+    <p class="feature-description">
+      <slot name="description" />
+    </p>
+  </a>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  link: string
+}>()
+</script>
+
+<style scoped src="./IconButton.css"></style>
