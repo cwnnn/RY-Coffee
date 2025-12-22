9ebc69f2796dc68aa8599fb16f1419de4ee35deb
cwnnn <cangs026@gmail.com>
2025-12-23 00:19:46 +0300

    add button component

---
+++ src/components/BaseButton/BaseButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    :type="type"
+    :disabled="disabled || loading"
+    :class="[
+      'btn',
+      `btn--${variant}`,
+      `btn--${color}`,
+      `btn--${size}`,
+      (disabled || loading) && 'is-disabled',
+    ]"
+  >
+    <span v-if="loading" class="btn__spinner" aria-hidden="true"></span>
+
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { BaseButtonProps } from './BaseButton.interface'
+
+withDefaults(defineProps<BaseButtonProps>(), {
+  variant: 'solid',
+  size: 'md',
+  color: 'primary',
+  disabled: false,
+  loading: false,
+  type: 'button',
+})
+</script>
+
+<style scoped src="./BaseButton.css"></style>
