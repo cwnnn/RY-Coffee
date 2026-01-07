6c45f4492ae3878fc10671bb7b611608de342b5a
cwnnn <cangs026@gmail.com>
2026-01-07 15:19:10 +0300

    add vueuse/motion

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n/index'
+import { MotionPlugin } from '@vueuse/motion'
 
 const app = createApp(App)
 
+app.use(MotionPlugin)
 app.use(router)
 app.use(i18n)
 app.mount('#app')
